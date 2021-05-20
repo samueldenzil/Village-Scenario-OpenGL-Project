83799256e0d4fea25088c90596c3b4987eaa4ee5
Denzil Samuel <71846487+samueldenzil@users.noreply.github.com>
2021-05-21 00:56:14 +0530

    Added main func

--- main.cpp
+++ main.cpp
@@ -15,3 +15,22 @@ void myInit(){
     glEnable(GL_LIGHT0); //enable light #0//
     glEnable(GL_COLOR_MATERIAL); //lets us use the glcolor3f//
 }
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(1200, 700);
+	
+	glutCreateWindow("Group 7: Village Scenario");
+	myInit();
+	
+	glutDisplayFunc(Display);
+	glutReshapeFunc(handleResize);
+
+	glutKeyboardFunc(keyboard);
+
+	glutTimerFunc(25, update, 0);	// Add a timer
+	glutMainLoop();
+	return 0;
+}
