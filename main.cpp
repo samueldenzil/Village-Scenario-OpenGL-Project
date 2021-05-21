b1bbd1e28ffad834cdce24ec5b4564e98ed8b6e1
Denzil Samuel <71846487+samueldenzil@users.noreply.github.com>
2021-05-21 17:53:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,114 @@ using namespace std;
 
 // PRESS 'n' FOR NIGHT VIEW AND PRESS 'd' FOR DAY VIEW //
 
-void myInit(){
-    glEnable(GL_DEPTH_TEST);
-    gluOrtho2D(0,1600,0,800);
-    glEnable(GL_LIGHTING); //enable lightining//
-    glEnable(GL_LIGHT0); //enable light #0//
-    glEnable(GL_COLOR_MATERIAL); //lets us use the glcolor3f//
+void myInit()
+{
+	glEnable(GL_DEPTH_TEST);
+	gluOrtho2D(0,1600,0,800);
+	glEnable(GL_LIGHTING); //enable lightining//
+	glEnable(GL_LIGHT0); //enable light #0//
+	glEnable(GL_COLOR_MATERIAL); //lets us use the glcolor3f//
+}
+
+void Sun()
+{
+	glColor3f(1.000, 0.843, 0.000);
+	circle(-0.25, 0.75, 0.18);
+}
+
+void Moon()
+{
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
+	circle(0.75, 0.75, 0.10);
+	glPopMatrix();
+}
+
+void Cloud1()
+{
+	glColor3f(0.80, 0.80, 0.80);
+	circle(-0.85, 0.77, 0.15);
+	circle(-0.79, 0.70, 0.15);
+	circle(-0.70, 0.70, 0.15);
+	circle(-0.75, 0.77, 0.15);
+}
+
+void Cloud2()
+{
+	glColor3f(0.80, 0.80, 0.80);
+	drawcircle(0.35, 0.77, 0.15);
+	circle(-0.79, 0.70, 0.15);
+	drawcircle(0.55, 0.75, 0.15);
+	circle(-0.75, 0.77, 0.15);
+}
+
+void Cloud3()
+{
+	glColor3f(0.80, 0.80, 0.80);
+	circle(-0.07, 0.85, 0.15);
+	circle(-0.01, 0.80, 0.15);
+	circle(0.08, 0.83, 0.15);
+	circle(0.02, 0.85, 0.15);
+}
+
+void Tree1()
+{
+	glColor3f(0.38f, 0.19f, 0.0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.25, -0.30);
+	glVertex2f(-0.25, -0.12);
+	glVertex2f(-0.27, -0.12);
+	glVertex2f(-0.27, -0.30);
+	glEnd();
+
+	glColor3f(0.0f, 0.43f, 0.0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.36, -0.12);
+	glVertex2f(-0.26, 0.12);
+	glVertex2f(-0.16, -0.12);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.36, -0.04);
+	glVertex2f(-0.26, 0.20);
+	glVertex2f(-0.16, -0.04);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.36, -0.04);
+	glVertex2f(-0.26, 0.28);
+	glVertex2f(-0.16, -0.04);
+	glEnd();
+}
+
+void Tree2()
+{
+	glColor3f(0.38f, 0.19f, 0.0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.05, -0.20);
+	glVertex2f(-0.05, -0.00);
+	glVertex2f(-0.07, -0.00);
+	glVertex2f(-0.07, -0.20);
+	glEnd();
+
+	glColor3f(0.0f, 0.43f, 0.0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.18, 0.00);
+	glVertex2f(-0.06, 0.24);
+	glVertex2f(-0.06, 0.00);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.18, 0.08);
+	glVertex2f(-0.06, 0.32);
+	glVertex2f(0.06, 0.08);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.18, 0.16);
+	glVertex2f(-0.06, 0.40);
+	glVertex2f(0.06, 0.16);
+	glEnd();
 }
 
 int main(int argc, char** argv)
